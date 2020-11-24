4c78b9f22bd28b3f815d5d8dca9b871af05885f4
Jonathan <mcjhona069@gmail.com>
2020-11-23 20:43:20 -0600

    AppBarLayout, Toolbar, y BottomNavigationView

---
+++ app/src/main/java/com/platzi/conferenciasapp/ui/activities/SplashscreenActivity.kt
@@ -0,0 +1,10 @@
+package com.platzi.conferenciasapp.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashscreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
