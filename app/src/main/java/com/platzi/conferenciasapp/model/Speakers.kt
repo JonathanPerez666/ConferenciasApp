02ec356787d6c1d1fe6fba4a34e37a7c6edd7c39
Jonathan <mcjhona069@gmail.com>
2020-11-24 13:58:14 -0600

    Conectando el proyecto con Firebase

---
+++ app/src/main/java/com/platzi/conferenciasapp/model/Speakers.kt
@@ -0,0 +1,10 @@
+package com.platzi.conferenciasapp.model
+
+class Speakers {
+    var name = ""
+    var jotitle = ""
+    var workplace = ""
+    var biography = ""
+    var twitter = ""
+    var image = ""
+}
