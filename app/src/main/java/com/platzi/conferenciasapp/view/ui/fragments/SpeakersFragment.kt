8202ef1f5a93b036b00d149a78e8ee310990176f
Jonathan <mcjhona069@gmail.com>
2020-11-24 20:17:38 -0600

    Configurando la App para ejecutar en dispositivo

--- app/src/main/java/com/platzi/conferenciasapp/view/ui/fragments/SpeakersFragment.kt
+++ app/src/main/java/com/platzi/conferenciasapp/view/ui/fragments/SpeakersFragment.kt
@@ -5,56 +5,60 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.platzi.conferenciasapp.R
+import com.platzi.conferenciasapp.model.Conference
+import com.platzi.conferenciasapp.model.Speaker
+import com.platzi.conferenciasapp.view.adapter.ScheduleAdapter
+import com.platzi.conferenciasapp.view.adapter.SpeakersAdapter
+import com.platzi.conferenciasapp.view.adapter.SpeakersListener
+import com.platzi.conferenciasapp.viewmodel.ScheduleViewModel
+import com.platzi.conferenciasapp.viewmodel.SpeakerViewModel
+import kotlinx.android.synthetic.main.fragment_schedule.*
+import kotlinx.android.synthetic.main.fragment_speakers.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class SpeakersFragment : Fragment(), SpeakersListener {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SpeakersFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class SpeakersFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var speakerAdpater: SpeakersAdapter
+    private lateinit var viewModel: SpeakerViewModel
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_speakers, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SpeakersFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SpeakersFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProviders.of(this).get(SpeakerViewModel::class.java)
+        viewModel.refresh()
+
+        speakerAdpater = SpeakersAdapter(this)
+
+        rwSpeakers.apply {
+            layoutManager = GridLayoutManager(context, 2)
+            adapter = speakerAdpater
+        }
+        observeViewModel()
     }
+
+    fun observeViewModel(){
+        viewModel.listSpeakers.observe(this, Observer<List<Speaker>> { speakers ->
+            speakers.let {
+                speakerAdpater.updateData(speakers)
+            }
+        })
+
+        viewModel.isLoading.observe(this, Observer<Boolean>{
+            if(it != null)
+                rlBaseSchedule.visibility = View.INVISIBLE
+        })
+    }
+
+    override fun onSpeakersClicked(speaker: Speaker, position: Int) {
+    }
+
+
 }
--- app/src/main/java/com/platzi/conferenciasapp/viewmodel/SpeakerViewModel.kt
+++ app/src/main/java/com/platzi/conferenciasapp/viewmodel/SpeakerViewModel.kt
@@ -1,13 +1,14 @@
 package com.platzi.conferenciasapp.viewmodel
 
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.platzi.conferenciasapp.model.Conference
 import com.platzi.conferenciasapp.model.Speaker
 import com.platzi.conferenciasapp.network.Callback
 import com.platzi.conferenciasapp.network.FirestoreService
 import java.lang.Exception
 
-class SpeakerViewModel {
+class SpeakerViewModel: ViewModel() {
     val firestoreService = FirestoreService()
     var listSpeakers: MutableLiveData<List<Speaker>> = MutableLiveData()
     var isLoading = MutableLiveData<Boolean>()
