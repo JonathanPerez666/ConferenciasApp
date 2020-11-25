c5bf3b156080acba425a6da092866b4ab3fb83df
Jonathan <mcjhona069@gmail.com>
2020-11-24 19:29:53 -0600

    Carga de datos desde el ViewModel

--- app/src/main/java/com/platzi/conferenciasapp/viewmodel/ScheduleViewModel.kt
+++ app/src/main/java/com/platzi/conferenciasapp/viewmodel/ScheduleViewModel.kt
@@ -1,12 +1,13 @@
 package com.platzi.conferenciasapp.viewmodel
 
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.platzi.conferenciasapp.model.Conference
 import com.platzi.conferenciasapp.network.Callback
 import com.platzi.conferenciasapp.network.FirestoreService
 import java.lang.Exception
 
-class ScheduleViewModel {
+class ScheduleViewModel: ViewModel() {
     val firestoreService = FirestoreService()
     var listSchedule: MutableLiveData<List<Conference>> = MutableLiveData()
     var isLoading = MutableLiveData<Boolean>()
