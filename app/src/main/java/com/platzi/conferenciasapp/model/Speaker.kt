f93d5647848b7861c9a635938417ea056175eac9
Jonathan <mcjhona069@gmail.com>
2020-11-24 20:55:55 -0600

    Envío y Recepción de datos a diálogos

--- app/src/main/java/com/platzi/conferenciasapp/model/Speaker.kt
+++ app/src/main/java/com/platzi/conferenciasapp/model/Speaker.kt
@@ -1,6 +1,8 @@
 package com.platzi.conferenciasapp.model
 
-class Speaker {
+import java.io.Serializable
+
+class Speaker: Serializable {
     var name = ""
     var jobtitle = ""
     var workplace = ""
