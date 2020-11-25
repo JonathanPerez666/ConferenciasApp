bc1eb5c21c8e98c21426c5b960a3bd16ac165217
Jonathan <mcjhona069@gmail.com>
2020-11-24 22:25:27 -0600

    Colocar estilos e información en un objeto mapa

--- app/src/main/java/com/platzi/conferenciasapp/view/ui/fragments/UbicationFragment.kt
+++ app/src/main/java/com/platzi/conferenciasapp/view/ui/fragments/UbicationFragment.kt
@@ -8,17 +8,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.findNavController
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.platzi.conferenciasapp.R
 import com.platzi.conferenciasapp.model.Ubication
 
-class UbicationFragment : Fragment(), OnMapReadyCallback {
+class UbicationFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_ubication, container, false)
@@ -50,7 +52,13 @@ class UbicationFragment : Fragment(), OnMapReadyCallback {
         markerOptions.icon(BitmapDescriptorFactory.fromBitmap(smallMarker))
 
         googleMap?.addMarker(markerOptions)
+        googleMap?.setOnMarkerClickListener(this)
 
+    }
+
+    override fun onMarkerClick(p0: Marker?): Boolean {
+        findNavController().navigate(R.id.ubicationDetailFragmentDialog)
+        return true
     }
 
 }
